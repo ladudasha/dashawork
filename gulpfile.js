c1458c66919f44eb169b36c1e4f99b33570cce63
ladudasha <darya.gameza@gmail.com>
2022-02-02 13:00:18 +0300

    gh pages

--- gulpfile.js
+++ gulpfile.js
@@ -65,9 +65,8 @@ const jsFiles = [
 
 gulp.task('deploy', function() {
     return gulp.src('./dist/**/*')
-        .pipe(ghPages());
-});
-
+      .pipe(ghPages());
+  });
 
 // Локальный сервер
 function serve() {
